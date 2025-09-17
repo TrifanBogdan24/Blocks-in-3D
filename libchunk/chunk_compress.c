e5d7f14ee436e48af0d63d726056302fea08ec74
TrifanBogdan24 <trifanbogdan24@gmail.com>
2025-09-17 07:14:20 +0300

    Solved task 9: encode

--- libchunk/chunk_compress.c
+++ libchunk/chunk_compress.c
@@ -2,9 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define B0 0
+#define B1 1
+#define B2 2
+#define B3 3
+#define B4 4
+#define B5 5
+#define B6 6
+#define B7 7
+#define B8 8
+#define B9 9
+#define B10 10
+#define B11 11
+
+
 
 void print_byte(unsigned char byte) {
-    for (int i = 7; i >= 0; i--)
+    for (int i = B7; i >= 0; i--)
         printf("%d", (byte & (1 << i)) ? 1 : 0);
 }
 
@@ -62,56 +76,56 @@ Pair *get_pairs(char *array, int length, int *num_pairs) {
 
 
 
+void add_pair_to_bytes(unsigned char **bytes, int *length,
+    Pair pair) {
+    int num_occurrences = pair.num_occurrences;
+    char block = pair.block;
 
-void or_btye_block(unsigned char *byte, char block) {
-    if (!byte)
-        return;
+
+    (*length) += 1;
+    *bytes = realloc(*bytes, (*length) * sizeof(unsigned char));
+    (*bytes)[*length - 1] = 0;
 
     // b1, b0
-    if (block == BLOCK_AIR || block == BLOCK_STONE) {
-        (*byte) |= block;
-    } else if (block == BLOCK_GRASS) {
-        (*byte) |= (1 << 1);
+    if (block == BLOCK_GRASS) {
+        (*bytes)[*length - 1] |= (1 << B6);
     } else if (block == BLOCK_WOOD) {
-        (*byte) |= 1;
+        (*bytes)[*length - 1] |= (1 << B7);
+    } else if (block == BLOCK_STONE) {
+        (*bytes)[*length - 1] |= (1 << B7);
+        (*bytes)[*length - 1] |= (1 << B6);
     }
-}
 
-void add_pair_to_bytes(unsigned char **bytes, int *length,
-    int num_occurences, char block) {
-    if (num_occurences < 32) {
+    if (num_occurrences < 32) {
+        // bb0nnnnn
+        if (num_occurrences & (1 << B4)) (*bytes)[*length - 1] |= (1 << B4);
+        if (num_occurrences & (1 << B3)) (*bytes)[*length - 1] |= (1 << B3);
+        if (num_occurrences & (1 << B2)) (*bytes)[*length - 1] |= (1 << B2);
+        if (num_occurrences & (1 << B1)) (*bytes)[*length - 1] |= (1 << B1);
+        if (num_occurrences & (1 << B0)) (*bytes)[*length - 1] |= (1 << B0);
+    } else {
+        // bb10nnnn nnnnnnnn
+        (*bytes)[*length - 1] |= (1 << B5);
+
         (*length) += 1;
         *bytes = realloc(*bytes, (*length) * sizeof(unsigned char));
         (*bytes)[*length - 1] = 0;
 
-        or_btye_block(&(*bytes)[*length - 1], block);
-        
-        for (int i = 4; i >= 0; i--) {
-            if (num_occurences >= 1 << i) {
-                num_occurences -= 1 << i;
-                (*bytes)[*length - 1] |= (1 << (7 - i));
-            }
-        }
-    } else {
-        (*length) += 2;
-        *bytes = realloc(*bytes, (*length) * sizeof(unsigned char));
-        (*bytes)[*length - 2] = 0;
-        (*bytes)[*length - 1] = 0;
-        or_btye_block(&(*bytes)[*length - 2], block);
+        // Primul octet:
+        if (num_occurrences & (1 << B11)) (*bytes)[*length - 2] |= (1 << B3);
+        if (num_occurrences & (1 << B10)) (*bytes)[*length - 2] |= (1 << B2);
+        if (num_occurrences & (1 << B9))  (*bytes)[*length - 2] |= (1 << B1);
+        if (num_occurrences & (1 << B8))  (*bytes)[*length - 2] |= (1 << B0);
 
-        (*bytes)[*length - 2] |= (1 << 2);
-
-        for (int i = 11; i >= 0; i--) {
-            if (num_occurences >= 1 << i) {
-                num_occurences -= 1 << i;
-
-                if (15 - i >= 8) {
-                    (*bytes)[*length - 1] |= 1 << (7 - i);
-                } else {
-                    (*bytes)[*length - 2] |= 1 << (15 - i);
-                }
-            }
-        }
+        // Al doilea octet:
+        if (num_occurrences & (1 << B7)) (*bytes)[*length - 1] |= (1 << B7);
+        if (num_occurrences & (1 << B6)) (*bytes)[*length - 1] |= (1 << B6);
+        if (num_occurrences & (1 << B5)) (*bytes)[*length - 1] |= (1 << B5);
+        if (num_occurrences & (1 << B4)) (*bytes)[*length - 1] |= (1 << B4);
+        if (num_occurrences & (1 << B3)) (*bytes)[*length - 1] |= (1 << B3);
+        if (num_occurrences & (1 << B2)) (*bytes)[*length - 1] |= (1 << B2);
+        if (num_occurrences & (1 << B1)) (*bytes)[*length - 1] |= (1 << B1);
+        if (num_occurrences & (1 << B0)) (*bytes)[*length - 1] |= (1 << B0);
     }
 }
 
@@ -125,21 +139,14 @@ unsigned char* chunk_encode(
     Pair *pairs = get_pairs(flat_arr, flat_arr_len, &num_pairs);
     free(flat_arr);
 
-    // TODO: continue
     (*length) = 0;
     unsigned char *bytes = NULL;
 
     for (int i = 0; i < num_pairs; i++) {
-        int num_occurrences = pairs[i].num_occurrences;
-        char block = pairs[i].block;
-
-        add_pair_to_bytes(&bytes, length, pairs[i].num_occurrences, pairs[i].block);
+        add_pair_to_bytes(&bytes, length, pairs[i]);
     }
 
     free(pairs);
-    for (int i = 0; i < *length; i++) {
-        printf("%2x\n", bytes[i]);
-    }
     return bytes;
 }
 
@@ -176,17 +183,17 @@ char*** chunk_decode(
     while (*ptr) {
         unsigned char byte = 0;
         char block = BLOCK_AIR;
-        int num_occurences = 0;
+        int num_occurrences = 0;
 
         byte = *ptr;
         ptr++;
-        if ((byte & (1 << 7)) && (byte & (1 << 6))) {
+        if ((byte & (1 << B7)) && (byte & (1 << B6))) {
             // b1b0 = 11
             block = BLOCK_STONE;
-        } else if ((byte & (1 << 7)) && !(byte & (1 << 6))) {
+        } else if ((byte & (1 << B7)) && !(byte & (1 << B6))) {
             // b1b0 = 10
             block = BLOCK_WOOD;
-        } else if (!(byte & (1 << 7)) && (byte & (1 << 6))) {
+        } else if (!(byte & (1 << B7)) && (byte & (1 << B6))) {
             // b1b0 = 01
             block = BLOCK_GRASS;
         } else {
@@ -194,37 +201,37 @@ char*** chunk_decode(
             block = BLOCK_AIR;
         }
 
-        if (!(byte & (1 << 5))) {
+        if (!(byte & (1 << B5))) {
             // bb0nnnnn
-            if (byte & (1 << 4)) num_occurences |= (1 << 4);
-            if (byte & (1 << 3)) num_occurences |= (1 << 3);
-            if (byte & (1 << 2)) num_occurences |= (1 << 2);
-            if (byte & (1 << 1)) num_occurences |= (1 << 1);
-            if (byte & (1 << 0)) num_occurences |= (1 << 0);
-        } else if ((byte & (1 << 5)) && !(byte & (1 << 4))) {
+            if (byte & (1 << B4)) num_occurrences |= (1 << B4);
+            if (byte & (1 << B3)) num_occurrences |= (1 << B3);
+            if (byte & (1 << B2)) num_occurrences |= (1 << B2);
+            if (byte & (1 << B1)) num_occurrences |= (1 << B1);
+            if (byte & (1 << B0)) num_occurrences |= (1 << B0);
+        } else if ((byte & (1 << B5)) && !(byte & (1 << B4))) {
             // bb10nnnn nnnnnnnn
 
-            // In primul octet:
-            if (byte & (1 << 3)) num_occurences |= (1 << 11);
-            if (byte & (1 << 2)) num_occurences |= (1 << 10);
-            if (byte & (1 << 1)) num_occurences |= (1 << 9);
-            if (byte & (1 << 0)) num_occurences |= (1 << 8);
+            // Primul octet:
+            if (byte & (1 << B3)) num_occurrences |= (1 << B11);
+            if (byte & (1 << B2)) num_occurrences |= (1 << B10);
+            if (byte & (1 << B1)) num_occurrences |= (1 << B9);
+            if (byte & (1 << B0)) num_occurrences |= (1 << B8);
 
-            // In al doilea octet:
+            // Al doilea octet:
             byte = *ptr;
             ptr++;
 
-            if (byte & (1 << 7)) num_occurences |= (1 << 7);
-            if (byte & (1 << 6)) num_occurences |= (1 << 6);
-            if (byte & (1 << 5)) num_occurences |= (1 << 5);
-            if (byte & (1 << 4)) num_occurences |= (1 << 4);
-            if (byte & (1 << 3)) num_occurences |= (1 << 3);
-            if (byte & (1 << 2)) num_occurences |= (1 << 2);
-            if (byte & (1 << 1)) num_occurences |= (1 << 1);
-            if (byte & (1 << 0)) num_occurences |= (1 << 0);
+            if (byte & (1 << B7)) num_occurrences |= (1 << B7);
+            if (byte & (1 << B6)) num_occurrences |= (1 << B6);
+            if (byte & (1 << B5)) num_occurrences |= (1 << B5);
+            if (byte & (1 << B4)) num_occurrences |= (1 << B4);
+            if (byte & (1 << B3)) num_occurrences |= (1 << B3);
+            if (byte & (1 << B2)) num_occurrences |= (1 << B2);
+            if (byte & (1 << B1)) num_occurrences |= (1 << B1);
+            if (byte & (1 << B0)) num_occurrences |= (1 << B0);
         }
 
-        for (int i = 0; i < num_occurences; i++) {
+        for (int i = 0; i < num_occurrences; i++) {
             chunk[x][y][z] = block;
 
             x++;
