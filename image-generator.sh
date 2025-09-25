b2ecd3d62d5ab4ca4a2c9b84939eaa479b49f143
TrifanBogdan24 <trifanbogdan24@gmail.com>
2025-09-25 14:23:32 +0300

    image-generator script show progress in CLI

--- image-generator.sh
+++ image-generator.sh
@@ -2,31 +2,48 @@
 
 METHOD=""   # "C"/"rs"
 
-MIN_TASK_IDX=1
-MAX_TASK_IDX=8
+MIN_idx_task=1
+MAX_idx_task=8
+
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m"
+BLUE="\033[1;34m"
+RESET="\033[0m"
+BLINK="\033[5m"
 
 function generate_task_images() {
-    task_idx=$1
+    idx_task=$1
 
-    if [[ ! -d images/task$task_idx/ ]] ; then
-        mkdir -p images/task$task_idx/
+    if [[ ! -d images/task$idx_task/ ]] ; then
+        mkdir -p images/task$idx_task/
     fi
 
-    rm -f images/task$task_idx/*-out.png
-    num_task_tests=$(ls "tests/input/task$task_idx/" | wc -l)
+    rm -f images/task$idx_task/*-out.png
+    num_task_tests=$(ls "tests/input/task$idx_task/" | wc -l)
 
     # Loop over each test index
-    local idx
     for (( idx=0; idx<num_task_tests; idx++ )); do
+        echo -n "Generating images for task$idx_task-test_$idx: "
+        
+        # Save cursor position, print flashing "pending"
+        tput sc
+        echo -ne "${YELLOW}${BLINK}pending${RESET}"
+
         # Don't generate the in/ref images if they already exist
-        if [[ ! -f images/task$task_idx/$idx-in.png ]] ; then
-            ./view3d.sh tests/input/task$task_idx/$idx.in -o images/task$task_idx/$idx-in.png
+        if [[ ! -f images/task$idx_task/$idx-in.png ]] ; then
+            ./view3d.sh tests/input/task$idx_task/$idx.in -o images/task$idx_task/$idx-in.png
         fi
-        if [[ ! -f images/task$task_idx/$idx-ref.png ]] ; then
-            ./view3d.sh tests/ref_output/task$task_idx/$idx.ref -o images/task$task_idx/$idx-ref.png
+        if [[ ! -f images/task$idx_task/$idx-ref.png ]] ; then
+            ./view3d.sh tests/ref_output/task$idx_task/$idx.ref -o images/task$idx_task/$idx-ref.png
         fi
 
-        ./view3d.sh tests-out/$METHOD/task$task_idx/$idx.out -o images/task$task_idx/$idx-out-$METHOD.png
+        ./view3d.sh tests-out/$METHOD/task$idx_task/$idx.out -o images/task$idx_task/$idx-out-$METHOD.png
+
+        # Restore cursor, overwrite with "DONE"
+        tput rc   
+        echo -ne "${BLUE}DONE   ${RESET}"
+
+        echo ""
     done
 }
 
@@ -41,7 +58,7 @@ function generate_all_images() {
 
     # Loop over each task index
     local idx
-    for (( idx=$MIN_TASK_IDX; idx<=$MAX_TASK_IDX; idx++ )); do
+    for (( idx=$MIN_idx_task; idx<=$MAX_idx_task; idx++ )); do
         generate_task_images $idx
     done
 }
@@ -83,7 +100,7 @@ case "$2" in
         ;;
     -t|--task)
         if [[ -n "$3" && "$3" =~ ^[0-9]+$ ]] ; then
-            if [[ "$3" -lt $MIN_TASK_IDX || "$3" -gt $MAX_TASK_IDX ]] ; then
+            if [[ "$3" -lt $MIN_idx_task || "$3" -gt $MAX_idx_task ]] ; then
                 echo "[ERROR] Invalid task index $3 to generate images for." >&2
                 exit 255
             fi
